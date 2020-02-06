4ba78e0f6452a041caaa0dd60dd4c02bb7c64b4d
Tchernobylette <pinku.lulu@hotmail.com>
2020-02-06 22:20:36 +0100

    :construction: first page

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 const image = document.querySelector(".image_js")
-document.addEventListener("scroll",()=>{
-  image.classList.add("image_scroll")
+document.addEventListener("scroll",(e)=>{
+  if(window.scrollY < window.innerHeight/2){
+    const scrollPercentage = (window.scrollY * 100)/window.innerHeight
+    image.style.transform = `scale(${1-(scrollPercentage*0.012)})`
+  }
 })
